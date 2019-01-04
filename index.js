04e763a67c68c5b0b609b5c0c7725a49d7b56b85
Paul Riverain <paulriverain@yahoo.com>
2019-01-04 19:02:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+//let katzDeliLine = ['jim', 'bob', 'carel', 'katie', 'sam','brenda', 'hanna','ray']
+
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
-    return 'Currently serving '+ katzDeliLine.shift() +'.'
+  if (katzDeliLine.length>0){
+    return 'Currently serving ' + katzDeliLine.shift()+'.'
   }else{
     return 'There is nobody waiting to be served!'
   }
 }
-let katzDeliLine = ['steve', 'mike', 'jim']
+
 function currentLine(katzDeliLine){
   let line = [];
   for(let i = 0; i<katzDeliLine.length; i++){
-    line.push(' '+[i+1]+'. '+katzDeliLine[i])
+    line.push(' '+ [1+i]+'. '+katzDeliLine[i])
+}
+    if (katzDeliLine.length>0){
+      return `The line is currently:` + line
   }
-  if (katzDeliLine.length >0){
-      return 'The line is currently:'+ line
-    }else{
-      return 'The line is currently empty.'
-    }
+  return 'The line is currently empty.'
 }
